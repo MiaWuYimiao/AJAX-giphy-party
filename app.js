554cfadadaa64eaf755c0f67ede90c2368939599
Yimiao Wu <wuyimiao51s@icloud.com>
2022-12-14 00:39:16 -0500

    create files for ajax giphy party

---
+++ app.js
@@ -0,0 +1,39 @@
+console.log("Let's get this party started!");
+
+const gifContainer = document.querySelector(".gifContainer");
+
+function addGif(imgSrc){
+    const newGif = document.createElement("img");
+    newGif.src= imgSrc;
+    gifContainer.append(newGif);
+}
+
+const form = document.querySelector("#form");
+form.addEventListener('submit',async function(evt){
+    evt.preventDefault();
+    // search for gif
+    const input = document.querySelector("#search-input");
+    const url = `http://api.giphy.com/v1/gifs/search?q=${input.value}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+    const res = await axios.get(url);
+    console.log(res.data.data[0]);
+    const resLen = res.data.data.length;
+    if(resLen>0){
+        let randomInd = Math.floor(Math.random()*resLen);
+        const imgSrc = res.data.data[randomInd].images.original.url;
+        // add gif
+        addGif(imgSrc);
+    }
+    input.value = "";
+})
+
+const rmvGif = document.querySelector("#removeBtn");
+rmvGif.addEventListener('click', function(){
+    console.log("remove clicked")
+    // remove all img
+    var child = gifContainer.lastElementChild;
+    while (child) {
+        gifContainer.removeChild(child);
+        child = gifContainer.lastElementChild;
+    }
+})
+
